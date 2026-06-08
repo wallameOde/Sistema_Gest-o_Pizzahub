205bf92f521882e06bd145da1c1d624f3a5367b5
wallame <horaciowallame@gmail.com>
2026-06-08 11:23:20 -0300

    Add ATUALIZAÇÃO STRUCT E VOID

--- cadastro_pizzaria.c
+++ cadastro_pizzaria.c
@@ -6,17 +6,49 @@
 #define MAX_PEDIDOS 100
 #define MAX_INGREDIENTES 5
 
+
 void limpar_buffer() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF) {}
 }
+typedef struct {
+    char nome[50];
+    float preco;
+    char ingredientes[MAX_INGREDIENTES][30];
+    int num_ingredientes;
+    int id;
+} Pizza;
+
+Pizza cardapio[MAX_PIZZAS];
+int total_pizzas = 0;
 
 void iniciar_cardapio() {
-    // Implementação da função
+strcpy(cardapio[0].nome, "Margherita");
+cardapio[0].preco = 30.00;
+cardapio[0].id = 1;
+total_pizzas++;
+
+strcpy(cardapio[1].nome, "Pepperoni");
+cardapio[1].preco = 35.00;
+cardapio[1].id = 2;
+total_pizzas++;
+
+strcpy(cardapio[2].nome, "Quatro Queijos");
+cardapio[2].preco = 40.00;
+cardapio[2].id = 3;
+total_pizzas++;
 }
 
 void mostrar_cardapio() {
-    // Implementação da função
+    printf("\n==== Cardapio ====\n");
+    int i;
+    
+    for ( i = 0; i < total_pizzas; i++){
+       printf("%d. %s - R$ %.2f\n", cardapio[i].id, cardapio[i].nome, cardapio[i].preco);
+    }
+    
+    
+
 }
 
 void fazer_pedido() {
